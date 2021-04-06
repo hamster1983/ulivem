90957f6636261dc10f83b4108fa82bc63e0036b3
Наймушин Егор <webmaster@farexpo.local>
2021-04-06 17:53:15 +0300

    vers 06.04

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 	
-	
+	/*$('#top-slider').on('slide.bs.carousel', function(e) {
+        $('.top-slider-indicators li').removeClass('active');
+        $('.top-slider-indicators li:nth-of-type('+(e.to+1)+')').addClass('active');
+    })*/
 	
 });
