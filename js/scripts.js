db13010e80dde8ed20f7c6a28a7d3539f7c30013
Наймушин Егор <webmaster@farexpo.local>
2021-04-13 17:08:29 +0300

    vers 13.04

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,30 @@ $(document).ready(function(){
         $('.top-slider-indicators li').removeClass('active');
         $('.top-slider-indicators li:nth-of-type('+(e.to+1)+')').addClass('active');
     })*/
+
+    $('.akc-slider').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+        speed: 1500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1199,
+            settings: {
+              slidesToShow: 2,
+            }
+          },
+          {
+            breakpoint: 820,
+            settings: {
+              slidesToShow: 1,
+            }
+          }
+        ] 
+      });
 	
 });
