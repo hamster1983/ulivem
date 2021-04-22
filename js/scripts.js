2eec35ad80299196c861d828782c4f456b7a58bc
Наймушин Егор <webmaster@farexpo.local>
2021-04-22 16:44:27 +0300

    vers 22.04

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
 	
-	/*$('#top-slider').on('slide.bs.carousel', function(e) {
-        $('.top-slider-indicators li').removeClass('active');
-        $('.top-slider-indicators li:nth-of-type('+(e.to+1)+')').addClass('active');
-    })*/
+	  //$('#top-slider').on('slide.bs.carousel', function(e) {
+        //$('.top-slider-indicators li').removeClass('active');
+        //$('.top-slider-indicators li:nth-of-type('+(e.to+1)+')').addClass('active');
+        //console.log(e.to)
+    //})
+
+    AOS.init();
 
     $('.akc-slider').slick({
         infinite: true,
